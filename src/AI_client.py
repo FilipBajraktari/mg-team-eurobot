8bdecc753d211fb1dfdb62f4d9b0890479a6e5de
set <set>
2023-04-11 16:05:32 +0200

    Added draft for templates

--- src/AI_client.py
+++ src/AI_client.py
@@ -1,8 +1,25 @@
 import time
 
+import threading
 import dbus
 import dbus.service
 import dbus.mainloop.glib
+from Behaviors.Behavior_template import *
+
+iface = None
+iface_ai = None
+CancelRequired = False
+
+def Action(x):
+    print("working")
+    global CancelRequired
+    StateSpace = None
+    while not (x.Complete):
+        x.ControlLoop(StateSpace,CancelRequired)
+    if x.Error:
+        print(x.Error)
+    return
+
 
 def main():
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
@@ -13,11 +30,25 @@ def main():
     iface = dbus.Interface(remote_object, "com.mgrobotics.OdometryInterface")
     iface_ai = dbus.Interface(remote_object, "com.mgrobotics.AI")
 
+
+
+    x = Template_Controller(iface = iface)
+    worker = None
     while True:
         # AI STUFF
         current_state_space = iface.get_random_state_space()
-        iface_ai.emit_new_desired_position([0,0,0])
+        # iface_ai.emit_new_desired_position([0,0,0])
+        if not worker:
+            worker = threading.Thread(target = Action, args=(Template_Controller(iface = iface),))
+            worker.daemon=True
+            worker.start()
+        if worker.is_alive() == False:
+            worker = None
+            print("Im finished ;)")
+        
+        print("Thinking...")
         time.sleep(1)
+        
 
 if __name__ == '__main__':
     main()
---
+++ src/Behaviors/Behavior_template.py
@@ -0,0 +1,12 @@
+from .Behavior import *
+
+class Template_Controller(Controller):
+    def Loop(self, x) -> None:
+        Contemp = self.iface.get_random_state_space()
+        print(f"{Contemp[0]}, {Contemp[1]}, {Contemp[2]}")
+        if Contemp[2] > 1:
+            self.Complete=True
+        return
+    def SafeExit(self, x) -> None:
+        print("No pls")
+        return
