b56a186bf59e035438e6cc5ff83b7fd49a6ccc3d
MainRpi <mgrobotics22@gmail.com>
2023-04-20 16:53:31 +0200

    Mega giga serial

---
+++ mega_serial.py
@@ -0,0 +1,42 @@
+import time
+import serial
+import numpy as np
+import serial.tools.list_ports
+
+
+# Init Pyserial
+ports = serial.tools.list_ports.comports()
+ser = serial.Serial()
+portList = []
+
+
+
+for p in ports:
+    portList.append(str(p))
+    print(str(p))
+
+
+#val = input("select port: /dev/ttyUSB")
+val = 0
+
+for x in range(0, len(portList)):
+    if portList[x].startswith("/dev/ttyUSB" + str(val)):
+        portVar = "/dev/ttyUSB" + str(val)
+
+ser.baudrate = 115200
+ser.port = portVar
+
+ser.open()
+
+while True:
+    command = input("Type in a command: ")
+    if(command == "close"):
+        ser.close()
+        break
+    ser.write(bytearray(command, 'ascii'))
+    time.sleep(0.1)
+    if ser.inWaiting():
+        s = ""
+        while ser.inWaiting():
+            s += ser.read().decode('ascii')
+        print(s)
